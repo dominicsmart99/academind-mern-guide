a1efa07b82bf8ae114840c4b491b5e3169cceed8
Dominic Smart <dominicsmart99@gmail.com.com>
2023-10-22 11:51:50 -0400

    Built out input component with validator

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,11 +1,18 @@
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 import "./NewPlace.css";
 
 function NewPlace() {
   return (
     <form className="place-form">
-      <Input type="text" label="Place Name" element="input" />
+      <Input
+        type="text"
+        label="Place Name"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 }
